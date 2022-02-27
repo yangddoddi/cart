17475125c312d4a3e39e5a86ba153194d07a6032
yangddoddi <pud1251@gmail.com>
2022-02-27 23:45:44 +0900

    JS__searchBox

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 "use strict";
 
+// 데이터 바인딩
+
 const shopLs = document.querySelector(".shop__list");
 const shopLsItem = document.querySelectorAll(".shop__list__item");
 const shopItemImg = document.querySelectorAll(".shop__list__item--img");
@@ -23,4 +25,18 @@ fetch("/store.json")
     }
   });
 
-// res를 빙글빙글 돌면서 제품명 브랜드 이미지 가격을 대입해주세요
+// 검색하면 원하는 상품만 보이게
+
+const searchBox = document.querySelector(".searchBox");
+
+searchBox.addEventListener("input", () => {
+  for (let i = 0; i < shopItemtitle.length; i++) {
+    if (shopItemtitle[i].innerHTML.includes(searchBox.value)) {
+      shopLsItem[i].classList.remove("hide");
+    } else {
+      shopLsItem[i].classList.add("hide");
+    }
+  }
+});
+
+// 서치박스에 메뉴 아이템의 제목에 들어가는 글자가 포함되는 것만 보여주십시오.
